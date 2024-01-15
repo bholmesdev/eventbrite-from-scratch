7c07f2f65339a0e60a40865abcfe41d16e1c8c68
bholmesdev <bholmesdev@gmail.com>
2024-01-15 17:42:56 -0500

    deps: simple-stack-form, zod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import cloudflare from "@astrojs/cloudflare";
-
 import simpleStackForm from "simple-stack-form";
 
 // https://astro.build/config

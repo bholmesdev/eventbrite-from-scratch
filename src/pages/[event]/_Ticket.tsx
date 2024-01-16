7f49424b4b4ec56a1dea6c130dfc09082b9acb3f
bholmesdev <bholmesdev@gmail.com>
2024-01-16 07:42:11 -0500

    feat: add price counter

--- src/pages/[event]/_Ticket.tsx
+++ src/pages/[event]/_Ticket.tsx
@@ -1,19 +1,30 @@
 import { createForm } from "simple:form";
 import { Form, Input } from "../../components/Form";
 import { z } from "zod";
+import { useState } from "preact/hooks";
 
 export const ticketForm = createForm({
   email: z.string().email(),
-  quantity: z.number(),
+  quantity: z.number().max(10),
   newsletter: z.boolean(),
 });
 
-export function TicketForm() {
+export function TicketForm({ price }: { price: number }) {
+  const [quantity, setQuantity] = useState(1);
   return (
     <>
       <Form validator={ticketForm.validator}>
+        <h3>${(quantity * price) / 100}</h3>
+
         <label for="quantity">Quantity</label>
-        <Input id="quantity" {...ticketForm.inputProps.quantity} />
+        <Input
+          id="quantity"
+          {...ticketForm.inputProps.quantity}
+          onInput={(e) => {
+            const value = Number(e.currentTarget.value);
+            setQuantity(value);
+          }}
+        />
 
         <label for="email">Email</label>
         <Input id="email" {...ticketForm.inputProps.email} />
